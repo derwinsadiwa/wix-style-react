7da64fabe2ab83c1f12bdaa1cb04428d1b14b691
argshook <argshook@gmail.com>
2019-01-25 08:36:10 +0200

    test(StatsWidget): use all examples for e2e

--- stories/StatsWidget/testStory.js
+++ stories/StatsWidget/testStory.js
@@ -6,7 +6,21 @@ import { getTestStoryKind } from '../storiesHierarchy';
 import { storySettings, testStories } from './storySettings';
 
 import Standard from './ExampleStatsWidgetStandard';
+import WithPercents from './ExampleStatsWidgetWithPercents';
+import WithInvertPercentColor from './ExampleStatsWidgetWithInvertPercentColor';
+import WithFilters from './ExampleStatsWidgetWithFilters';
+import EmptyState from './ExampleStatsWidgetEmptyState';
+import WithFilterWithNoBorder from './ExampleStatsWidgetWithFilterWithNoBorder';
 
 const kind = getTestStoryKind(storySettings);
 
-storiesOf(kind, module).add(testStories.statsWidget, () => <Standard />);
+storiesOf(kind, module).add(testStories.statsWidget, () => (
+  <div>
+    <Standard />
+    <WithPercents />
+    <WithInvertPercentColor />
+    <WithFilterWithNoBorder />
+    <WithFilters />
+    <EmptyState />
+  </div>
+));
